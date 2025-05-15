b7dc5a6580f3c3fda6781b3314c28a8a85db5530
gandharpansare <gandharpansare@gmail.com>
2025-05-15 16:06:42 -0400

    Consumer code

---
+++ weather_consumer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    'weather-data',
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+for message in consumer:
+    weather_data = message.value
+    print("Received:", weather_data)
